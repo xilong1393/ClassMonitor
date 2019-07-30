64d4e805ba131ac5ddc6d618954aa5da396f8b37
Yu <XYU42@depaul.edu>
2019-07-30 11:08:28 -0500

    deleted all

--- Model/ParameterForm/GetClassroomListForm.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ClassMonitor3.Models
-{
-    public class GetClassroomListForm
-    {
-        public string SessionID { get; set; }
-        public int GroupID { get; set; }
-    }
-}
--- Model/ParameterForm/UserLoginForm.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ClassMonitor3.Models
-{
-    public class UserLoginForm
-    {
-        public string UserName { get; set; }
-        public string Password { get; set; }
-    }
-}
